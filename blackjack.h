9a2b55492bc0e96570ac51f22da82d7ce7b3a001
hamzaswati <swatih@oregonstate.edu>
2020-08-09 16:41:31 -0700

    Update blackjack.h

--- blackjack.h
+++ blackjack.h
@@ -1,1 +1,16 @@
-//declare functions here
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NUMOFCARDS 52
+
+
+struct card {
+  int value;
+  char* suit;
+}
+
+struct Deck{
+  card *crd;
+  struct Deck *next;
+}
