f1be990c78dc4195be5126a607d9a1437a91d3b4
akshatlunia <64396583+akshatlunia@users.noreply.github.com>
2020-08-11 17:58:56 -0700

    Update main.c

--- main.c
+++ main.c
@@ -27,16 +27,34 @@ int main(){
     game = gameInit(game);
     game = createPlayers(game, 1);
     while(again == 1){
+        //creates 1 player to start the game with
+        game = createPlayers(game, 1);
         deck = shuffleDeck(deck);
         if(chips < 0){
             printf("You have gone bankrupt!");
             again = 0;
+            //keep in mind this will go right to delete, which is what
+            //it should do. This will end the program though.
         }
         else{
         printf("Would you like to play again? (Enter 1 for yes, 0 for no): ");
         scanf("%d", &again); 
+        deck->top_card_num = 0;
+        deck->top_card = deck->cards[deck->top_card_num];
+        deletePlayers(game);
         }
     }
+    /*
+    *
+    * 
+    * To add a new card to a player, pass the game object, deck
+    * object, and the number 1 because we will only have
+    * one player in our game
+    * 
+    * 
+    * 
+    */
+
     deleteDeck(deck);
     deletePlayers(game);
     deleteGame(game);
